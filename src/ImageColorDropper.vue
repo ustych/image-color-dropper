d9bd5711960c22cc426e40ce4b0f28ce7d420638
m.ustych <m.ustych@ideil.com>
2019-09-27 17:01:20 +0300

    add logic for generate hex color

---
+++ src/ImageColorDropper.vue
@@ -0,0 +1,118 @@
+<template>
+    <canvas ref="canvas" />
+</template>
+
+<script>
+export default {
+    props: {
+        imageId: {
+            type: [ String ],
+            required: true
+        }
+    },
+    data () {
+        return {
+            image: '',
+            body: document.body
+        }
+    },
+    methods: {
+        draw () {
+            this.image = document.getElementById(this.imageId)
+
+            this.image.addEventListener('load', () => {
+                let canvas = this.$refs.canvas
+
+                canvas.width = this.image.width
+                canvas.height = this.image.height
+
+                canvas
+                    .getContext('2d')
+                    .drawImage(
+                        this.image,
+                        0,
+                        0,
+                        this.image.width,
+                        this.image.height,
+                        0,
+                        0,
+                        canvas.width,
+                        canvas.height
+                    )
+
+                this.image.remove()
+            })
+        },
+        handleMouseOnCanvas (event) {
+            event.preventDefault()
+            event.stopPropagation()
+
+            if (this.body.style.cursor) {
+                return
+            }
+
+            this.body.style.cursor = 'url("data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAABZklEQVRIibXVu0odURjF8Z+XxhSiYJVAciovUSHBkDSaoE/gC9gGfIBgEbSyFsR3SIoUFukjpBOsNFYiCiFeQY0aUFGPxd5DDoN6ZsZxwWaYb5j/WvtjXyhHffiOUxzhKyolsb3HCaqpsYsXD4V/EFKn4cn4UgTaFJ9DdeBVHDTmhA/gF17iN/YzhskMP4jJdqJJBRvunsF8HoOfqZ+30HWPyTF68hi8xUUKkszkOdZr6v8wnAfehT/Yw2odk8LwHfSiHUt3mFQwmAfeje04umvq95k8GJ6oA2spkxVkWvrpttymARziLMIP8SYLvLNOcngnHGpr6McPvMoCf4rNmDwL/FkWaKKGmOQvXpcNJ6zdKj4+BhymhZ3a8hjwJ8IuXLjl24jQtsLwRuFiuBIukEStmMA5loUFUEhzQu8/x/dRLOIy1r+hrSgcxoT085j1f0dOCSdoKRrHdYTPoLkscK0+YbJs6A0RmY1aTL+JrQAAAABJRU5ErkJggg=="), auto'
+
+        },
+        handleMouseLeaveCanvas (event) {
+            event.preventDefault()
+            event.stopPropagation()
+
+            this.body.style.cursor = ''
+        },
+        getElementPosition(obj) {
+            var curleft = 0, curtop = 0
+            if (obj.offsetParent) {
+                do {
+                    curleft += obj.offsetLeft
+                    curtop += obj.offsetTop
+                } while (obj == obj.offsetParent)
+
+                return { x: curleft, y: curtop }
+            }
+
+            return undefined
+        },
+        getEventLocation(element, event){
+            var pos = this.getElementPosition(element)
+
+            return {
+                x: (event.pageX - pos.x),
+                y: (event.pageY - pos.y)
+            }
+        },
+        rgbToHex(r, g, b) {
+            if (r > 255 || g > 255 || b > 255)
+                throw "Invalid color component";
+            return ((r << 16) | (g << 8) | b).toString(16);
+        },
+    },
+    mounted () {
+        this.draw()
+
+        const canvas = this.$refs.canvas
+
+        canvas.addEventListener('mousemove', event => {
+            this.handleMouseOnCanvas(event)
+        })
+
+        canvas.addEventListener('mouseleave', event => {
+            this.handleMouseLeaveCanvas(event)
+        })
+
+        canvas.addEventListener('click', event => {
+                let eventLocation = this.getEventLocation(canvas, event)
+                let context       = canvas.getContext('2d')
+                let pixelData     = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data
+
+                if ((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)){
+                    // Do something if the pixel is transparent
+                }
+
+                let hex = "#" + ("000000" + this.rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6)
+
+                this.$emit('hex', hex)
+            }, false)
+    }
+}
+</script>
---
+++ src/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img id="image-to-replace" src="./assets/cake1.jpg" />
+        <ImageColorDropper
+            imageId="image-to-replace"
+            @hex="showHex"
+        />
+    </div>
+</template>
+
+<script>
+import ImageColorDropper from './ImageColorDropper.vue'
+
+export default {
+    components: {
+        ImageColorDropper
+    },
+    methods: {
+        showHex (hex) {
+            console.log(hex)
+        }
+    }
+}
+</script>
